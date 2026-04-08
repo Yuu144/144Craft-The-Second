4336e457972bc76eb7fa5bc31f5047cbc6b18c39
Ikaros144 <shizukaros@gmx.net>
2026-04-08 23:34:08 +0200

    mekanism crafting

---
+++ kubejs/server_scripts/mekanism.js
@@ -0,0 +1,122 @@
+ServerEvents.recipes(event => {
+
+    event.remove({ output: 'mekanism:metallurgic_infuser' })
+    event.remove({ output: 'mekanismgenerators:bio_generator' })
+    event.remove({ output: 'mekanismgenerators:gas_burning_generator' })
+    event.remove({ output: 'mekanism:electrolytic_core' })
+    event.remove({ output: 'bfr:fusion_reactor_controller' })
+
+    event.shaped('mekanism:metallurgic_infuser', [
+        'ACA',
+        'BDB',
+        'ACA'
+    ], {
+        A: 'minecraft:iron_ingot',
+        B: 'minecraft:redstone',
+        C: 'alltheores:osmium_ingot',
+        D: 'create_sa:hydraulic_engine'
+    })
+
+    event.shaped('mekanismgenerators:bio_generator', [
+        'ABA',
+        'CDC',
+        'EBE'
+    ], {
+        A: 'minecraft:redstone',
+        B: 'mekanism:alloy_infused',
+        C: '#c:fuels/bio',
+        D: 'mekanism:basic_control_circuit',
+        E: 'alltheores:steel_ingot'
+    })
+
+    event.shaped('mekanismgenerators:gas_burning_generator', [
+        'ABA',
+        'CDC',
+        'ABA'
+    ], {
+        A: 'alltheores:osmium_ingot',
+        B: 'mekanism:alloy_infused',
+        C: 'mekanismgenerators:bio_generator',
+        D: 'mekanism:electrolytic_core'
+    })
+
+    event.custom({
+        "type": "create:sequenced_assembly",
+        "ingredient": {
+            "item": "mekanism:alloy_reinforced"
+        },
+        "loops": 1,
+        "results": [
+            {
+                "id": "mekanism:electrolytic_core"
+            }
+        ],
+        "sequence": [
+            {
+                "type": "create:deploying",
+                "ingredients": [
+                    {
+                        "item": "mekanism:alloy_reinforced"
+                    },
+                    {
+                        "tag": "c:dusts/osmium"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "mekanism:alloy_reinforced"
+                    }
+                ]
+            },
+            {
+                "type": "create:deploying",
+                "ingredients": [
+                    {
+                        "item": "mekanism:alloy_reinforced"
+                    },
+                    {
+                        "tag": "c:dusts/iron"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "mekanism:alloy_reinforced"
+                    }
+                ]
+            },
+            {
+                "type": "create:deploying",
+                "ingredients": [
+                    {
+                        "item": "mekanism:alloy_reinforced"
+                    },
+                    {
+                        "tag": "c:dusts/gold"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "mekanism:alloy_reinforced"
+                    }
+                ]
+            }
+        ],
+        "transitional_item": {
+            "id": "mekanism:alloy_reinforced"
+        }
+
+    })
+
+    event.shaped('bfr:fusion_reactor_controller', [
+        'ABA',
+        'CDC',
+        'CCC'
+    ], {
+        A: 'mekanism:ultimate_control_circuit',
+        B: 'tfmg:circuit_board',
+        C: 'bfr:fusion_reactor_frame',
+        D: 'mekanism:ultimate_chemical_tank'
+    })
+
+
+})
