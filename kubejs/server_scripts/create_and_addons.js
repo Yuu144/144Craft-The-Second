dc266fe9ac4b9efa53e88bbc3ed1cce963ea3ef7
Ikaros144 <shizukaros@gmx.net>
2026-04-13 00:12:24 +0200

    quests and more recipes

--- kubejs/server_scripts/create_and_addons.js
+++ kubejs/server_scripts/create_and_addons.js
@@ -1,6 +1,8 @@
 ServerEvents.recipes(event => {
     
     event.remove({ output: 'dndesires:hydraulic_press' })
+    event.remove({ output: 'create:copper_casing' })
+    event.remove({ output: 'create:railway_casing' })
 
     event.shaped('dndesires:hydraulic_press', [
         ' A ',
@@ -20,6 +22,134 @@ ServerEvents.recipes(event => {
         A: '#minecraft:wooden_slabs',
         B: 'create:andesite_casing',
         C: 'create:shaft'
+    })
+
+    event.custom({
+        "type": "create:sequenced_assembly",
+        "ingredient": {
+            "item": "create:andesite_casing"
+        },
+        "loops": 1,
+        "results": [
+            {
+                "id": "create:copper_casing"
+            }
+        ],
+        "sequence": [
+            {
+                "type": "create:deploying",
+                "ingredients": [
+                    {
+                        "item": "create:andesite_casing"
+                    },
+                    {
+                        "item": "minecraft:copper_ingot"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "create:andesite_casing"
+                    }
+                ]
+            },
+            {
+                "type": "create:deploying",
+                "ingredients": [
+                    {
+                        "item": "create:andesite_casing"
+                    },
+                    {
+                        "item": "minecraft:copper_ingot"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "create:andesite_casing"
+                    }
+                ]
+            },
+            {
+                "type": "create:pressing",
+                "ingredients": [
+                    {
+                        "item": "create:andesite_casing"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "create:andesite_casing"
+                    }
+                ]
+            }
+        ],
+        "transitional_item": {
+            "id": "create:andesite_casing"
+        }
+
+    })
+
+    event.custom({
+        "type": "create:sequenced_assembly",
+        "ingredient": {
+            "item": "create:brass_casing"
+        },
+        "loops": 1,
+        "results": [
+            {
+                "id": "create:railway_casing"
+            }
+        ],
+        "sequence": [
+            {
+                "type": "create:deploying",
+                "ingredients": [
+                    {
+                        "item": "create:brass_casing"
+                    },
+                    {
+                        "item": "create:sturdy_sheet"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "create:andesite_casing"
+                    }
+                ]
+            },
+            {
+                "type": "create:deploying",
+                "ingredients": [
+                    {
+                        "item": "create:brass_casing"
+                    },
+                    {
+                        "item": "create:sturdy_sheet"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "create:andesite_casing"
+                    }
+                ]
+            },
+            {
+                "type": "create:pressing",
+                "ingredients": [
+                    {
+                        "item": "create:brass_casing"
+                    }
+                ],
+                "results": [
+                    {
+                        "id": "create:brass_casing"
+                    }
+                ]
+            }
+        ],
+        "transitional_item": {
+            "id": "create:brass_casing"
+        }
+
     })
 
 })
