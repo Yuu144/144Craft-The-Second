32fd71bbb8afb5fa000cc1c94199abea0dfc2aa9
Ikaros144 <shizukaros@gmx.net>
2026-04-13 22:57:58 +0200

    more scripts

---
+++ kubejs/server_scripts/oritech.js
@@ -0,0 +1,27 @@
+ServerEvents.recipes(event => {
+
+    //event.custom({
+
+    //   "type": "oritech:particle_collision",
+    //        "fluidInput": {
+    //        "amount": 0,
+    //        "fluid": "minecraft:empty"
+    //    },
+    //        "fluidOutputs": [],
+    //        "ingredients": [
+    //   {
+    //        "item": "minecraft:redstone"
+    //    },
+    //    {
+    //        "item": "minecraft:glowstone"
+    //    }
+    //    ],
+    //        "results": [
+    //    {
+    //        "count": 1,
+    //        "id": "kubejs:strange_matter"
+    //    }
+    //    ],
+    //        "time": 5000
+//    })
+})
