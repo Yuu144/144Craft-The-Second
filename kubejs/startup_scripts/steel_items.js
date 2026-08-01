246ed72ab00cf7d79f7e91d978020edfdf1e1c60
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-01 23:41:16 +0200

    Changes

---
+++ kubejs/startup_scripts/steel_items.js
@@ -0,0 +1,4 @@
+    StartupEvents.registry('item', event => {
+        event.create('pig_iron_ingot').displayName('Pig Iron Ingot')
+        event.create('unforged_steel_ingot').displayName('Unforged Steel Ingot')
+    })
