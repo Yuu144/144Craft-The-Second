62f30593b01d585b1ea999e938e390618088fd00
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-13 21:28:04 +0200

    Steel Recipe

--- kubejs/server_scripts/greg_type_recipes/steel_recipes.js
+++ kubejs/server_scripts/greg_type_recipes/steel_recipes.js
@@ -3,10 +3,11 @@ ServerEvents.recipes(event => {
     event.remove({ output: '#c:dusts/steel' })
     event.remove({ output: '#c:dusts/coal' })
     event.remove({ output: 'alltheores:alloy_smelting/steel_ingot' })
-    event.remove({ output: 'tfmg:molten_steel' })
     event.remove({ type: 'alltheores:alloy_smelting', output: 'alltheores:steel_ingot' })
     event.remove({ id: 'oritech:compat/enderio/alloy/steel' })
-
+    event.remove({ id: 'tfmg:industrial_blasting/steel' })
+    event.remove({ id: 'tfmg:vat_machine_recipe/arc_furnace_steel' })
+    
     // Explicitly remove all recipes for alternative coal dusts to prevent bypasses
     event.remove({ output: 'enderio:powdered_coal' })
     event.remove({ output: 'oritech:coal_dust' })
