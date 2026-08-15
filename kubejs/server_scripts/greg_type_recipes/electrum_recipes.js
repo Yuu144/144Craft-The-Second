90b7e02de0656d6377e99b33f4f404a482dca1fe
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-15 15:43:28 +0200

    Electrum Recipe

--- kubejs/server_scripts/greg_type_recipes/electrum_recipes.js
+++ kubejs/server_scripts/greg_type_recipes/electrum_recipes.js
@@ -1,16 +1,16 @@
 ServerEvents.recipes(event => {
-    event.remove({ not: { output: "alltheores:electrum_ingot" }, output: '#c:ingots/electrum' })
-    event.remove({ not: { output: "alltheores:electrum_dust" }, output: '#c:dusts/electrum' })
-    event.remove({ id: 'alltheores:crafting/electrum/alloy_blending_from_dust' })
-    event.remove({ id: 'alltheores:alloy_smelting/electrum_ingot' })
-    event.remove({ id: 'alltheores:alloysmelter/electrum/ingot' })
-    event.remove({ id: 'alltheores:arcfurnace/electrum/ingot' })
-    event.remove({ id: 'alltheores:sag_milling/electrum/ingot' })
-    event.remove({ id: 'electroenergetics:crafting/electrum_wire' })
-    event.remove({ type: 'minecraft:smelting', input: '#c:dusts/electrum' })
-    event.remove({ type: 'minecraft:blasting', input: '#c:dusts/electrum' })
-    event.remove({ output: 'alltheores:electrum_dust' })
-    event.remove({ output: 'createaddition:electrum_sheet' })
+    event.remove({ output: 'alltheores:electrum_ingot' })
+    //event.remove({ output: 'alltheores:electrum_dust' })
+    //event.remove({ output: '#c:dusts/electrum' })
+    //event.remove({ id: 'alltheores:crafting/electrum/alloy_blending_from_dust' })
+    //event.remove({ id: 'alltheores:alloy_smelting/electrum_ingot' })
+    //event.remove({ id: 'alltheores:alloysmelter/electrum/ingot' })
+    //event.remove({ id: 'alltheores:arcfurnace/electrum/ingot' })
+    //event.remove({ id: 'alltheores:sag_milling/electrum/ingot' })
+    //event.remove({ id: 'electroenergetics:crafting/electrum_wire' })
+    //event.remove({ type: 'minecraft:smelting', input: '#c:dusts/electrum' })
+    //event.remove({ type: 'minecraft:blasting', input: '#c:dusts/electrum' })
+    //event.remove({ output: 'createaddition:electrum_sheet' })
 
     event.custom({
         type: 'tfmg:vat_machine_recipe',
