ad439f0b75a4e525141a6ad85fd82ee25c1eec9b
Ikaros144 <shizukaros@gmx.net>
2026-08-16 12:36:49 +0200

    constantan recipes

---
+++ kubejs/startup_scripts/constantan_items.js
@@ -0,0 +1,5 @@
+StartupEvents.registry('item', event => {
+    event.create('manganese')
+        .displayName('manganese')
+        .tooltip('§7An extremely small amount of manganese in powder form')
+})
