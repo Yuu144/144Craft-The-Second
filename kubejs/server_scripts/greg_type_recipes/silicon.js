ac0232c16b10645c95f78f2460830261a1bfeffc
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-17 17:07:43 +0200

    Silicon

--- kubejs/server_scripts/greg_type_recipes/silicon.js
+++ kubejs/server_scripts/greg_type_recipes/silicon.js
@@ -4,6 +4,45 @@ ServerEvents.tags('item', event => {
 })
 
 ServerEvents.recipes(event => { 
+    event.custom({
+        type: 'tfmg:vat_machine_recipe',
+        allowed_vat_types: [
+            'tfmg:firebrick_lined_vat'
+        ],
+        ingredients: [
+            { tag: 'c:dusts/certus_quartz' },
+            { tag: 'c:dusts/certus_quartz' },
+            { tag: 'c:dusts/coal' },
+            { tag: 'c:dusts/coal' }
+        ],
+        machines: [
+            'tfmg:graphite_electrode',
+            'tfmg:graphite_electrode',
+            'tfmg:graphite_electrode'
+        ],
+        min_size: 9,
+        processing_time: 20,
+        results: [
+            {
+                amount: 500,
+                id: 'tfmg:liquid_silicon'
+            }
+        ]
+    })
 
+    event.custom({
+        type: 'tfmg:casting',
+        ingredients: [
+            {
+                type: 'neoforge:single',
+                amount: 250,
+                fluid: 'tfmg:liquid_silicon'
+            }
+        ],
+        processing_time: 200,
+        results: [
+            { id: 'ae2:silicon' }
+        ]
+    })
 })
 
