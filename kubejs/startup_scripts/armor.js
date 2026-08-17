35564f0577c4262ea1e45c202026e810d8912a56
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-17 11:09:13 +0200

    Armor

--- kubejs/startup_scripts/armor.js
+++ kubejs/startup_scripts/armor.js
@@ -1,55 +1,74 @@
+// KubeJS 7 / Minecraft 1.21 uses the item attribute-modifier component.
+// The old armorProtection, armorToughness and armorKnockbackResistance fields
+// are not available in this version.
+function setArmorAttributes(item, slot, protection, toughness, knockbackResistance) {
+    var modifiers = item.item().getDefaultAttributeModifiers()
+        .withModifierAdded(
+            'minecraft:generic.armor',
+            {
+                operation: 0,
+                amount: protection,
+                id: 'armor'
+            },
+            slot
+        )
+        .withModifierAdded(
+            'minecraft:generic.armor_toughness',
+            {
+                operation: 0,
+                amount: toughness,
+                id: 'armor_toughness'
+            },
+            slot
+        )
+        .withModifierAdded(
+            'minecraft:generic.knockback_resistance',
+            {
+                operation: 0,
+                amount: knockbackResistance,
+                id: 'knockback_resistance'
+            },
+            slot
+        )
+
+    // Keep the modifiers visible in the item's tooltip.
+    item.setAttributeModifiersWithTooltip(modifiers.modifiers())
+}
+
 ItemEvents.modification(event => {
     event.modify('cataclysm_spellbooks:ignis_helmet', item => {
-        item.armorProtection = 5
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'head', 5, 3, 0.1)
     })
 
     event.modify('cataclysm_spellbooks:ignis_chestplate', item => {
-        item.armorProtection = 10
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'chest', 10, 3, 0.1)
     })
 
     event.modify('cataclysm_spellbooks:ignis_chestplate_elytra', item => {
-        item.armorProtection = 10
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'chest', 10, 3, 0.1)
     })
 
     event.modify('cataclysm_spellbooks:ignis_leggings', item => {
-        item.armorProtection = 8
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'legs', 8, 3, 0.1)
     })
 
     event.modify('cataclysm_spellbooks:ignis_boots', item => {
-        item.armorProtection = 5
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'feet', 5, 3, 0.1)
     })
 
     event.modify('forbidden_arcanus:tyr_helmet', item => {
-        item.armorProtection = 5
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'head', 5, 3, 0.1)
     })
 
     event.modify('forbidden_arcanus:tyr_chestplate', item => {
-        item.armorProtection = 11
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'chest', 11, 3, 0.1)
     })
 
     event.modify('forbidden_arcanus:tyr_leggings', item => {
-        item.armorProtection = 8
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'legs', 8, 3, 0.1)
     })
 
     event.modify('forbidden_arcanus:tyr_boots', item => {
-        item.armorProtection = 5
-        item.armorToughness = 3
-        item.armorKnockbackResistance = 0.1
+        setArmorAttributes(item, 'feet', 5, 3, 0.1)
     })
 })
