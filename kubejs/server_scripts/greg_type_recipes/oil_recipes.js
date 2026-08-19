9ca01f1d87e676258e93b00abdcaf53c3ead22da
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-19 10:35:48 +0200

    Recipes

--- kubejs/server_scripts/greg_type_recipes/oil_recipes.js
+++ kubejs/server_scripts/greg_type_recipes/oil_recipes.js
@@ -5,6 +5,7 @@ ServerEvents.recipes(event => {
     event.remove({ id: 'modern_industrialization:petrochem/distillation/crude_oil_0' })
     event.remove({ id: 'modern_industrialization:petrochem/distillation/crude_oil_1' })
     event.remove({ id: 'modern_industrialization:petrochem/distillation/crude_oil_2' })
+
     event.custom({
         type: "modern_industrialization:distillery",
         duration: 200,
@@ -81,6 +82,54 @@ ServerEvents.recipes(event => {
             {
                 amount: 300,
                 fluid: "modern_industrialization:sulfuric_naphtha"
+            }
+        ]
+    })
+
+    event.remove({ id: 'tfmg:hot_blast/hot_air' })
+    event.custom({
+        type: "tfmg:hot_blast",
+        ingredients: [
+            {
+                type: "neoforge:single",
+                amount: 25,
+                fluid: "tfmg:air"
+            },
+            {
+                type: "neoforge:single",
+                amount: 5,
+                fluid: "modern_industrialization:creosote"
+            }
+        ],
+        processing_time: 200,
+        results: [
+            {
+                amount: 25,
+                id: "tfmg:hot_air"
+            },
+            {
+                amount: 25,
+                id: "tfmg:carbon_dioxide"
+            }
+        ]
+    })
+
+    event.remove({ id: 'tfmg:filling/hardened_planks' })
+    event.custom({
+        type: "create:filling",
+        ingredients: [
+            {
+                tag: "minecraft:planks"
+            },
+            {
+                type: "neoforge:single",
+                amount: 250,
+                fluid: "modern_industrialization:creosote"
+            }
+        ],
+        results: [
+            {
+                id: "tfmg:hardened_planks"
             }
         ]
     })
