b654fe696abe4bb7a9152f6214970c237c200985
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-19 16:23:46 +0200

    Recipes & Multiblock

--- kubejs/server_scripts/greg_type_recipes/mekanism_circuits.js
+++ kubejs/server_scripts/greg_type_recipes/mekanism_circuits.js
@@ -102,4 +102,49 @@ ServerEvents.recipes(event => {
             }
         ]
     })
+
+    event.remove({ output: 'mekanism:elite_control_circuit' })
+    event.custom({
+        type: "modern_industrialization:circuit_assembler",
+        duration: 50,
+        eu: 16,
+        item_inputs: [
+            {
+                amount: 1,
+                item: "mekanism:advanced_control_circuit"
+            },
+            {
+                amount: 2,
+                item: "modern_industrialization:diode"
+            },
+            {
+                amount: 3,
+                tag: "c:ingots/electrum"
+            },
+            {
+                amount: 3,
+                tag: "c:ingots/signalum"
+            },
+            {
+                amount: 2,
+                tag: "modern_industrialization:op_amp"
+            },
+            {
+                amount: 1,
+                tag: "modern_industrialization:electronic_circuit_board"
+            }
+        ],
+        fluid_inputs: [
+            {
+                fluid: "modern_industrialization:hydrochloric_acid",
+                amount: 500
+            }
+        ],
+        item_outputs: [
+            {
+                amount: 1,
+                item: "mekanism:advanced_control_circuit"
+            }
+        ]
+    })
 })
