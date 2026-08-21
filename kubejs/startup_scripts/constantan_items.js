eb5fe544c11939da5808858f17ee0c90300d1989
Ikaros144 <shizukaros@gmx.net>
2026-08-21 21:49:30 +0200

    constantan final

--- kubejs/startup_scripts/constantan_items.js
+++ kubejs/startup_scripts/constantan_items.js
@@ -1,5 +1,1 @@
-StartupEvents.registry('item', event => {
-    event.create('manganese')
-        .displayName('manganese')
-        .tooltip('§7An extremely small amount of manganese in powder form')
-})
+//there is nothing
