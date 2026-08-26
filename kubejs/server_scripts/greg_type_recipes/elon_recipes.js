be4b9f8c1714494022b00208dbc26d184eb48a60
Ikaros144 <shizukaros@gmx.net>
2026-08-26 16:12:02 +0200

    elon recipes

--- kubejs/server_scripts/greg_type_recipes/elon_recipes.js
+++ kubejs/server_scripts/greg_type_recipes/elon_recipes.js
@@ -1,8 +1,10 @@
 ServerEvents.recipes(event => {
+
+    //EST-Larp Alloy
     event.custom({
         type: "modern_industrialization:tesla",
-        duration: 50,
-        eu: 12800,
+        duration: 250,
+        eu: 50000,
         item_inputs: [
             {
                 amount: 8,
@@ -21,6 +23,37 @@ ServerEvents.recipes(event => {
             {
                 amount: 1,
                 item: "kubejs:energized_superconducting_titanium_larp_alloy"
+            }
+        ]
+    })
+
+    //Infinity Range Booster
+        event.custom({
+        type: "modern_industrialization:tesla",
+        duration: 100,
+        eu: 15000,
+        item_inputs: [
+            {
+                amount: 4,
+                item: "ae2:wireless_booster"
+            },
+            {
+                amount: 16,
+                item: "alltheores:enderium_ingot"
+            },
+            {
+                amount: 1,
+                item: "minecraft:nether_star"
+            },
+            {
+                amount: 2,
+                item: "ae2:quantum_entangled_singularity"
+            }
+        ],
+        item_outputs: [
+            {
+                amount: 1,
+                item: "aeinfinitybooster:infinity_card"
             }
         ]
     })
