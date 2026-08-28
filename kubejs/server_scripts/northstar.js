60829c1ac16d8bdfa3c31439762de5c4dc81d41a
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2026-08-28 21:31:12 +0200

    Recipes

--- kubejs/server_scripts/northstar.js
+++ kubejs/server_scripts/northstar.js
@@ -22,4 +22,46 @@ ServerEvents.recipes(event => {
         'modern_industrialization:titanium_ingot',
         'modern_industrialization:neodymium_dust'
     )
+
+    event.replaceInput(
+        { id: 'northstar:mechanical_crafting/rocket_waypoint' },
+        'modern_industrialization:titanium_ingot',
+        'minecraft:netherite_ingot'
+    )
+
+    event.replaceInput(
+        { id: 'northstar:mechanical_crafting/rocket_waypoint' },
+        'modern_industrialization:titanium_plate',
+        'alltheores:netherite_plate'
+    )
+
+    event.replaceInput(
+        { id: 'northstar:sequenced_assembly/hardened_precision_mechanism' },
+        'modern_industrialization:titanium_nugget',
+        'modern_industrialization:tungsten_double_ingot'
+    )
+
+    event.replaceInput(
+        { id: 'northstar:crafting/rocket_thruster' },
+        'modern_industrialization:titanium_plate',
+        'mekanism:steel_casing'
+    )
+
+    event.replaceInput(
+        { id: 'northstar:mechanical_crafting/rocket_station' },
+        'modern_industrialization:titanium_ingot',
+        'alltheores:netherite_plate'
+    )
+
+    event.replaceInput(
+        { id: 'northstar:mechanical_crafting/rocket_station' },
+        'modern_industrialization:titanium_plate',
+        'modern_industrialization:tungsten_plate'
+    )
+
+    event.replaceInput(
+        { id: 'northstar:crafting/space_atlas' },
+        'modern_industrialization:titanium_ingot',
+        'modern_industrialization:tungsten_plate'
+    )
 })
