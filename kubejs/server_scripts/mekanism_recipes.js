df500b8820d16702f9f3b8489bb8fceb20b11db1
Ikaros144 <shizukaros@gmx.net>
2026-08-30 13:41:12 +0200

    komm lass mich

--- kubejs/server_scripts/mekanism_recipes.js
+++ kubejs/server_scripts/mekanism_recipes.js
@@ -12,7 +12,7 @@ ServerEvents.recipes(event => {
     event.remove({ output: 'mekanism:crusher' })
     event.remove({ output: 'mekanismgenerators:wind_generator' })
     event.remove({ output: 'mekanismgenerators:heat_generator' })
-    event.remove({ mod: 'industrial_foregoing', output: 'mekanism:luthium_dust' })
+    event.remove({ mod: 'industrialforegoing', output: 'mekanism:lithium_dust' })
     //event.remove({ output: 'mekanism:heavy_water' })
 
     event.shaped('mekanism:metallurgic_infuser', [
